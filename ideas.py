70c1b4c963bd2ab373b1893065e01c93a920ab8c
HPLegion <32222819+HPLegion@users.noreply.github.com>
2018-02-06 09:02:51 +0100

    Add more functions

--- ideas.py
+++ ideas.py
@@ -1,6 +1,11 @@
-#Trivial import statements
-import numpy as np
+"""
+This document contains a collection of functions to help with the creation of secondary electrons
+for particle collisions with model surfaces for CST simulation studio
+"""
+
+# Trivial import statements
 import sys
+import numpy as np
 from matplotlib import pyplot as plt
 
 #Load FreeCAD
@@ -19,15 +24,16 @@ def rotate_about_axis(vector, axis, angle):
     The sense of rotation follows the right hand rule
     The rotation is implemented using the Euler Rodriguez Formula
     See: https://en.wikipedia.org/wiki/Euler%E2%80%93Rodrigues_formula
+    vector and axis should be numpy arrays of length 3
     """
-    # Perform a very rough check that vector and axis have the right dimensions
-    assert(vector.size == 3)
-    assert(axis.size == 3)
+    # Perform a check that vector and axis have the right dimensions
+    assert vector.shape == (3, ) or vector.shape == (1, 3) 
+    assert axis.shape == (3, ) or axis.shape == (1, 3)
 
     # Compute helper values
     phi = angle / 2
     norm_ax = axis / np.linalg.norm(axis)
-    
+
     # Compute Euler Rodrigues parameters
     a = np.cos(phi)
     b = np.sin(phi) * norm_ax[0]
@@ -38,7 +44,7 @@ def rotate_about_axis(vector, axis, angle):
     rot_mat = np.array([[a*a+b*b-c*c-d*d, 2*(b*c-a*d), 2*(b*d+a*c)],
                         [2*(b*c+a*d), a*a-b*b+c*c-d*d, 2*(c*d-a*b)],
                         [2*(b*d-a*c), 2*(c*d+a*b), a*a-b*b-c*c+d*d]])
-    
+
     # Perform rotation and return result
     return rot_mat.dot(vector)
 
@@ -62,12 +68,14 @@ def rotate_about_z(vector, angle):
     The sense of rotation follows the right hand rule
     """
     return rotate_about_axis(vector, np.array([0, 0, 1]), angle)
-    
+
 def angle_between(vec1, vec2):
-    """ 
+    """
     Returns the angle between two vectors in radians
     """
-    assert(vec1.size == vec2.size)
+    # Perform a check that vector and axis have the right dimensions
+    assert vec1.shape == (3, ) or vec1.shape == (1, 3) 
+    assert vec2.shape == (3, ) or vec2.shape == (1, 3)
 
     v1 = vec1 / np.linalg.norm(vec1)
     v2 = vec2 / np.linalg.norm(vec2)
@@ -75,13 +83,13 @@ def angle_between(vec1, vec2):
 
 def import_trajectory_file(filename):
     """
-    Imports a CST trajectory file, may apply filters to the trajectories immediately while importing,
-    returns a list with particle impact trajectories
+    Imports a CST trajectory file, may apply filters to the trajectories immediately while
+    importing, returns a list with particle impact trajectories
     (A list of tuples containing the last and 2nd to last recorded position of each particle, this
     should be sufficient to approximate the impact position and angle)
     """
-    raise NotImplementedError
     raise RuntimeWarning("May use problem specific import filter")
+    raise NotImplementedError
 
 def load_model(filename):
     """
@@ -89,18 +97,33 @@ def load_model(filename):
     """
     return Part.read(filename)
 
+def create_line(start, stop):
+    """
+    Creates and returns a FreeCAD line from a given set of start and stop coordinates
+    Expects start and stop to be numpy arrays
+    """
+    assert start.shape == (3, ) or start.shape == (1, 3)
+    assert stop.shape == (3, ) or stop.shape == (1, 3)
+    assert all(np.not_equal(start,stop)), "Start and stop coordinate should not be identical"
+
+    start = tuple(start)
+    stop = tuple(stop)
+    return Part.makeLine(start, stop)
+
 
 def intersection_with_model(line, model, atol=1e-6):
     """
-    finds the intersection of a line (list of two points) with the provided model object,
-    returns a tuple consisting of the collision location and the surface normal vector at that position
+    Finds the intersection of a FreeCAD line object with the provided model object
+    Returns a tuple consisting of the collision location and the surface normal vector at that
+    position
     atol defines the maximum distance between line and model to be a valid collision.
     """
     raise NotImplementedError
 
 def generate_secondaries(primary, model):
     """
-    Uses the line defined by the end of a primary trajectory and the model to generate secondary electrons
+    Uses the line defined by the end of a primary trajectory and the model to generate
+    secondary electrons
     """
     # Get primary collision information
     raise NotImplementedError
